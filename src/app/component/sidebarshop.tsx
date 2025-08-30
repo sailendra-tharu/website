c09abbb81afa968622e4260b7d9ecd9942835016
sailendra <sailendradastharu2000@gmail.com>
2025-08-30 20:14:59 +0545

    fixing some issue

--- src/app/component/sidebarshop.tsx
+++ src/app/component/sidebarshop.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 export default function ShopSideBar() {
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className="inline-block border border-gray-200 rounded p-4">
